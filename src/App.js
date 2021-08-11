0c40dfb48eed9ad710da095ec5f2ecf7cc522e38
Juan Pablo <juanpablo.solisrodriguez@gmail.com>
2021-08-11 15:13:24 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <p>Prueba de aplicacion en Git</p>
         <a
           className="App-link"
           href="https://reactjs.org"
