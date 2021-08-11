9afca0dc831d12d4d902eb14c9909368be31cf07
Juan Pablo <juanpablo.solisrodriguez@gmail.com>
2021-08-11 15:35:45 -0400

    agrego mas texto

--- src/App.js
+++ src/App.js
@@ -10,6 +10,8 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <p>Prueba de aplicacion en Git</p>
+
+        <p>Prueba de cambio de texto, para que me confirmen</p>
         <a
           className="App-link"
           href="https://reactjs.org"
